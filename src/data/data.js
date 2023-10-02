119b61b44373e5280bdfb20ec576514e709939c4
Lnkeyd <the.lnkeyd@gmail.com>
2023-10-02 18:14:46 +0300

    initial commit

---
+++ src/data/data.js
@@ -0,0 +1,21 @@
+data = [
+    {
+        id: 1,
+        price: 522,
+        currency: 'сом',
+        oldPrice: 1022,
+        oldCurrency: 'com',
+        name: 'asdfasdf',
+        color: '',
+        size: '',
+        trademark: '',
+        stockName: '',
+        stockCount: '',
+
+    }
+]
+
+order = [
+    // itemCount
+    // 
+]
